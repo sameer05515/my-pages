bc59a592772e84b88fb5dd436f2ef082c1193aac
Premendra.Kumar <premendra.kumar@indiabulls.com>
2020-01-06 19:12:48 +0530

    Commited for change
    -
    
    Style for pre section added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var app = angular.module('DemoApp', []);
 
 app.constant("AppConfig", {
-	"showLinkData" : true,
-	"showDbBakupLinkData":true,
+	"showLinkData" : false,
+	"showDbBakupLinkData":false,
 	"restServices" : "http://127.0.0.1:8080/RestServices/rest",
 	"interviewMgmtServices":"http://127.0.0.1:8080/interview-mgmt/rest",
 });
