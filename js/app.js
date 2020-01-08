22243b0f939d15ab920b5b420963d5ef98cf1a65
Premendra.Kumar <premendra.kumar@indiabulls.com>
2020-01-08 13:08:42 +0530

    Added few additional git commands

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('DemoApp', []);
 
 app.constant("AppConfig", {
 	"showLinkData" : false,
-	"showDbBakupLinkData":false,
+	"showDbBakupLinkData":true,
 	"restServices" : "http://127.0.0.1:8080/RestServices/rest",
 	"interviewMgmtServices":"http://127.0.0.1:8080/interview-mgmt/rest",
 });
