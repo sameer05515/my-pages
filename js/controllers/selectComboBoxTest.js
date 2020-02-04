5e0278f138391ae32c31ccc5dd016eeeb7dfb9b3
Premendra.Kumar <premendra.kumar@indiabulls.com>
2020-02-04 13:10:10 +0530

    Committed for private/personal mark for a question

--- js/controllers/selectComboBoxTest.js
+++ js/controllers/selectComboBoxTest.js
@@ -18,12 +18,34 @@ app.controller('selectComboBoxTest', function ($scope, $http, $log,AppConfig) {
 		$scope.error = response.status;
 		$log.log("Error : status " + response.status + " data " + response.data + " headers " + response.headers + " config " + response.config);
 	}
+	
+	$scope.showOnlySelectedCategoriesQuestions=function(value){
+		var returnVal=false;
+		
+		angular.forEach($scope.categoryData, function (val, key) { 
+		
+		
+			//$log.log(">>>>>>>>>>>>>>>>>val "+angular.toJson(val));
+			$log.log(">>>>>>>>>>>>>>>>>val "+angular.toJson(value));
+			$log.log(">>>>>>>>>>>>>>>>>(val.catID==value.catID)&&(val.showQuestionsOfCategory) "+((val.catID==value.catID)&&(val.showQuestionsOfCategory)));
+                if((val.catID==value.catID)&&(val.showQuestionsOfCategory)){
+					return true;
+					return;
+				} 
+            });
+		
+		//return false;
+	};
 
 	$scope.init = function () {
 		$http.get(AppConfig.interviewMgmtServices+"/categories").then(
 			function (response) {
 				$scope.categoryData = response.data;
-				$log.log("Succsss : status " + response.status + " data " + angular.toJson(response.data));
+				angular.forEach($scope.categoryData, function (val, key) { 
+					val.showQuestionsOfCategory=true;
+				});
+				
+				//$log.log("Succsss : status " + response.status + " data " + angular.toJson(response.data));
 			}, onError);
 		$http.get("data/json/stylesData.json").then(
 			function (response) {
@@ -31,6 +53,8 @@ app.controller('selectComboBoxTest', function ($scope, $http, $log,AppConfig) {
 				//$log.log("Succsss : status " + response.status + " data " + angular.toJson(response.data));
 			}, onError);
 	};
+	
+	
 
 	$scope.init();
 
