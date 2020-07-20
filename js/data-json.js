7c407b6f0f3d1d8a902a44220e8d9753548a05b7
Sameer.05515 <sameer.geeee@gmail.com>
2020-07-20 23:59:25 +0530

    committing at Mon 07/20/2020 23:59:06.74

--- js/data-json.js
+++ js/data-json.js
@@ -2,8 +2,9 @@ var app = angular.module("testApp", ["ngSanitize", "ui.bootstrap"]);
 
 app.controller("testCtrl", [
   "$scope",
-  "$http","$log",
-  function ($scope,  $http,$log) {
+  "$http",
+  "$log",
+  function ($scope, $http, $log) {
     // $scope.alert = function () {
     //   console.log('Alert clicked');
     //   $scope.alertOpen = true;
@@ -40,55 +41,86 @@ app.controller("testCtrl", [
       );
     };
 
-    $scope.clicklinkData = function (urll,dataType) {
+    $scope.clicklinkData = function (urll, dataType) {
       //let urll = "";
-      $log.log("urll : "+urll);
-      $http.get(urll).then(function(response){
-        
+      $log.log("urll : " + urll);
+      $http.get(urll).then(function (response) {
         // $log.log("response recieved : "+angular.toJson(response));
-        switch(dataType){
-          case 'LINK_DATA': $scope.loadLinkData(response); break;
-          case 'dbBakupLinkData': $scope.loadDbBakupLinkData(response); break;
-          case 'importResponse': $scope.loadImportResponse(response); break;
-          case 'stylesData': $scope.loadStylesData(response); break;
-          case 'IrregularVerb': $scope.loadIrregularVerb(response); break;
-          case 'wordRoots': $scope.loadWordRoots(response); break;
-          case 'wordSuffixes': $scope.loadWordSuffixes(response); break;
-          default : break;
-
+        switch (dataType) {
+          case "LINK_DATA":
+            $scope.loadLinkData(response);
+            break;
+          case "dbBakupLinkData":
+            $scope.loadDbBakupLinkData(response);
+            break;
+          case "importResponse":
+            $scope.loadImportResponse(response);
+            break;
+          case "stylesData":
+            $scope.loadStylesData(response);
+            break;
+          case "IrregularVerb":
+            $scope.loadIrregularVerb(response.data);
+            break;
+          case "wordRoots":
+            $scope.loadWordRoots(response);
+            break;
+          case "wordSuffixes":
+            $scope.loadWordSuffixes(response);
+            break;
+          default:
+            break;
         }
-      },onError);
+      }, onError);
     };
 
-    $scope.loadLinkData = function(response){
-      $log.log("response recieved : "+angular.toJson(response));
+    $scope.loadLinkData = function (response) {
+      $log.log("response recieved : " + angular.toJson(response));
     };
 
-    $scope.loadDbBakupLinkData = function(response){
-      $log.log("response recieved : "+angular.toJson(response));
+    $scope.loadDbBakupLinkData = function (response) {
+      $log.log("response recieved : " + angular.toJson(response));
     };
 
-    $scope.loadImportResponse = function(response){
-      $log.log("response recieved : "+angular.toJson(response));
+    $scope.loadImportResponse = function (response) {
+      $log.log("response recieved : " + angular.toJson(response));
     };
 
-    $scope.loadStylesData = function(response){
-      $log.log("response recieved : "+angular.toJson(response));
+    $scope.loadStylesData = function (response) {
+      $log.log("response recieved : " + angular.toJson(response));
     };
 
-    $scope.loadIrregularVerb = function(response){
-      $log.log("response recieved : "+angular.toJson(response));
+    $scope.loadIrregularVerb = function (response) {
+      $log.log("response recieved : " + angular.toJson(response));
+
+      for (let i = 0; i < response.length; i++) {
+        //{"V1Infinitive":"creep","V2SimplePast":"crept","V3PastParticiple":"crept"}
+        let obj = response[i];
+        document.writeln(
+          "INSERT INTO t_irregular_verb" +
+            "(id, unique_name, V1Infinitive, V2SimplePast, V3PastParticiple, example)" +
+            "VALUES ( \"" +
+            (i+1) +
+            "\",\"" +
+            obj.V1Infinitive +
+            (i+1) +
+            "\",\"" +
+            obj.V1Infinitive +
+            "\",\"" +
+            obj.V2SimplePast +
+            "\",\"" +
+            obj.V3PastParticiple +
+            '\",\"\");' +'<br>'
+        );
+      }
     };
 
-    $scope.loadWordRoots = function(response){
-      $log.log("response recieved : "+angular.toJson(response));
+    $scope.loadWordRoots = function (response) {
+      $log.log("response recieved : " + angular.toJson(response));
     };
 
-    $scope.loadWordSuffixes = function(response){
-      $log.log("response recieved : "+angular.toJson(response));
+    $scope.loadWordSuffixes = function (response) {
+      $log.log("response recieved : " + angular.toJson(response));
     };
-
-
-
   },
 ]);
