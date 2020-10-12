e669ba20e46f144af454c9dee6685aeba9097f66
Sameer.05515 <sameer.geeee@gmail.com>
2020-10-12 16:12:01 +0530

    committing at Mon 10/12/2020 16:11:20.98

--- other-sample-application/graph-data-representation/D3chartWithAngularJS-LineChart/js/directive/directive.graph.js
+++ other-sample-application/graph-data-representation/D3chartWithAngularJS-LineChart/js/directive/directive.graph.js
@@ -5,18 +5,20 @@
 		.directive('d3GraphRepresentation', ['d3',
 			function (d3) {
 				return {
-					restrict: 'EA',
-					link: function (scope) {
-						var data = [100, 25, 250, 300, 150,
-							300, 200, 400, 200, 80, 450];
-						var data2 = [0, 0, 0, 0, 0, 0, 0, 0,
-							0, 0, 0, 0, 0];
-						var width = 800, height = 500, margin = 45;
-						// creating a div to contain line charts.
-						var div = d3.select('body').append('div')
-							.style('border', 'solid black');
+					restrict: 'A',
+					scope: {
+						src: '=',
+						parent:'='
+					},
+					link: function (scope,elem, attrs) {
+						console.info("enter directive controller");
+						console.info("scope.parent : "+scope.parent);
+						var data = {};
+						var width = 300, height = 300, margin = 45;
+						var div = d3.select('#'+scope.parent).style("border", "solid green")
+							.style("width", "400px").style("height", "400px");
 						var svg = div.append('svg:svg')
-							.attr('class', 'graph-respresentation')
+							.attr('class', 'lines-respresentation')
 							.attr('width', width)
 							.attr('height', height)
 							.style('background-color', '#eee');
@@ -28,162 +30,21 @@
 							return angular.element(window)[0].innerWidth;
 						},
 							function () {
+								// return scope.render(scope.data);
 								return scope.render(scope.data);
 							});
+
 						// define render function
 						scope.render = function (d) {
+							var g = svg.append("svg:circle")
+								.attr('cx', '50')
+								.attr('cy', '50')
+								.attr('r', '40')
+								.attr('stroke', 'green')
+								.attr('stroke-width', '4')
+								.attr('fill', 'yellow');
+						}
 
-							var colors = d3.scaleOrdinal(d3.schemeCategory10);
-
-							var svg = d3.select("svg"),
-								width = +svg.attr("width"),
-								height = +svg.attr("height"),
-								node,
-								link;
-
-							svg.append('defs').append('marker')
-								.attrs({
-									'id': 'arrowhead',
-									'viewBox': '-0 -5 10 10',
-									'refX': 13,
-									'refY': 0,
-									'orient': 'auto',
-									'markerWidth': 13,
-									'markerHeight': 13,
-									'xoverflow': 'visible'
-								})
-								.append('svg:path')
-								.attr('d', 'M 0,-5 L 10 ,0 L 0,5')
-								.attr('fill', '#999')
-								.style('stroke', 'none');
-
-							var simulation = d3.forceSimulation()
-								.force("link", d3.forceLink().id(function (d) { return d.id; }).distance(100).strength(1))
-								.force("charge", d3.forceManyBody())
-								.force("center", d3.forceCenter(width / 2, height / 2));
-
-							// d3.json("graph.json", function (error, graph) {
-							//     if (error) throw error;
-							//     update(graph.links, graph.nodes);
-							// })
-
-							d3.json("../../../directed-graph-d3-js/d3-direc-wtd/familyTreeFromDB.json", function (error, graph) {
-								if (error) throw error;
-								update(graph.links, graph.nodes);
-							})
-
-							function update(links, nodes) {
-								link = svg.selectAll(".link")
-									.data(links)
-									.enter()
-									.append("line")
-									.attr("class", "link")
-									.attr('marker-end', 'url(#arrowhead)')
-
-								link.append("title")
-									.text(function (d) { return d.type; });
-
-								edgepaths = svg.selectAll(".edgepath")
-									.data(links)
-									.enter()
-									.append('path')
-									.attrs({
-										'class': 'edgepath',
-										'fill-opacity': 0,
-										'stroke-opacity': 0,
-										'id': function (d, i) { return 'edgepath' + i }
-									})
-									.style("pointer-events", "none");
-
-								edgelabels = svg.selectAll(".edgelabel")
-									.data(links)
-									.enter()
-									.append('text')
-									.style("pointer-events", "none")
-									.attrs({
-										'class': 'edgelabel',
-										'id': function (d, i) { return 'edgelabel' + i },
-										'font-size': 10,
-										'fill': '#aaa'
-									});
-
-								edgelabels.append('textPath')
-									.attr('xlink:href', function (d, i) { return '#edgepath' + i })
-									.style("text-anchor", "middle")
-									.style("pointer-events", "none")
-									.attr("startOffset", "50%")
-									.text(function (d) { return d.type });
-
-								node = svg.selectAll(".node")
-									.data(nodes)
-									.enter()
-									.append("g")
-									.attr("class", "node")
-									.call(d3.drag()
-										.on("start", dragstarted)
-										.on("drag", dragged)
-										//.on("end", dragended)
-									);
-
-								node.append("circle")
-									.attr("r", 5)
-									.style("fill", function (d, i) { return colors(i); })
-
-								node.append("title")
-									.text(function (d) { return d.id; });
-
-								node.append("text")
-									.attr("dy", -3)
-									.text(function (d) { return d.name + ":" + d.label; });
-
-								simulation
-									.nodes(nodes)
-									.on("tick", ticked);
-
-								simulation.force("link")
-									.links(links);
-							}
-
-							function ticked() {
-								link
-									.attr("x1", function (d) { return d.source.x; })
-									.attr("y1", function (d) { return d.source.y; })
-									.attr("x2", function (d) { return d.target.x; })
-									.attr("y2", function (d) { return d.target.y; });
-
-								node
-									.attr("transform", function (d) { return "translate(" + d.x + ", " + d.y + ")"; });
-
-								edgepaths.attr('d', function (d) {
-									return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y;
-								});
-
-								edgelabels.attr('transform', function (d) {
-									if (d.target.x < d.source.x) {
-										var bbox = this.getBBox();
-
-										rx = bbox.x + bbox.width / 2;
-										ry = bbox.y + bbox.height / 2;
-										return 'rotate(180 ' + rx + ' ' + ry + ')';
-									}
-									else {
-										return 'rotate(0)';
-									}
-								});
-							}
-
-							function dragstarted(d) {
-								if (!d3.event.active) simulation.alphaTarget(0.3).restart()
-								d.fx = d.x;
-								d.fy = d.y;
-							}
-
-							function dragged(d) {
-								d.fx = d3.event.x;
-								d.fy = d3.event.y;
-							}
-
-						};
 					}
 				};
 			}]);
