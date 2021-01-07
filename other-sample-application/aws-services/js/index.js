bd5810fb834973d8543614a610067a70b6858ac8
Premendra.Kumar <premendra.bce05515@gmail.com>
2021-01-07 20:07:54 +0530

    committing at Thu Jan  7 20:07:24 IST 2021

--- other-sample-application/aws-services/js/index.js
+++ other-sample-application/aws-services/js/index.js
@@ -1,60 +1,74 @@
 var app = angular.module("awsServicesApp", [
-  "ngTouch",
-  "ngAnimate",
-  "ui.bootstrap",
+    "ngTouch",
+    "ngAnimate",
+    "ui.bootstrap",
+    "ngRoute",
+]);
+
+app.config(function($routeProvider) {
+    $routeProvider.when("/one/:paramValue", { templateUrl: "one.html" });
+    $routeProvider.when("/two", { templateUrl: "two.html" });
+});
+
+app.controller("oneCtrl", [
+    "$scope", "$http", "$routeParams",
+    function($scope, $http, $routeParams) {
+        $scope.myRouteParamValue = $routeParams.paramValue;
+    }
 ]);
 
 app.controller("imgController", [
-  "$scope",
-  "$http",
-  function ($scope, $http) {
-    $scope.myText = "I am working";
-    $scope.error = "";
-    $scope.servicesOverviewData = [];
+    "$scope",
+    "$http",
+    function($scope, $http) {
+        $scope.myText = "I am working";
+        $scope.error = "";
+        $scope.servicesOverviewData = [];
 
-    $scope.imageData = [
-      {
-        imageLink: "data/images/img-007.jpg",
-        alternativeText: "Sample Social Media Application",
-        captions: ["Sample Social Media Application"],
-      },
-      {
-        imageLink: "data/images/img-006.jpg",
-        alternativeText: "Sample Social Media Application : Implementation using AWS services",
-        captions: ["Sample Social Media Application : Implementation using AWS services"]
-      },
-      {
-        imageLink: "data/images/img-001.jpg",
-        alternativeText: "AWS Security services",
-        captions: ["Pictorial diagram for AWS Security Services"],
-      },
-      {
-        imageLink: "data/images/img-013.jpg",
-        alternativeText: "AWS Global Data Centres",
-        captions: ["Pictorial diagram for AWS Global Data Centres"],
-      },
-    ];
+        $scope.imageData = [{
+                imageLink: "data/images/img-007.jpg",
+                alternativeText: "Sample Social Media Application",
+                captions: ["Sample Social Media Application"],
+            },
+            {
+                imageLink: "data/images/img-006.jpg",
+                alternativeText: "Sample Social Media Application : Implementation using AWS services",
+                captions: [
+                    "Sample Social Media Application : Implementation using AWS services",
+                ],
+            },
+            {
+                imageLink: "data/images/img-001.jpg",
+                alternativeText: "AWS Security services",
+                captions: ["Pictorial diagram for AWS Security Services"],
+            },
+            {
+                imageLink: "data/images/img-013.jpg",
+                alternativeText: "AWS Global Data Centres",
+                captions: ["Pictorial diagram for AWS Global Data Centres"],
+            },
+        ];
 
-    //   $scope.refreshServicesOverviewData=function (){
-    //     $http.get("data/raw-services-overview-2.json").then(function (response) {
-    //       $scope.servicesOverviewData = response.data;
-    //     }, onError);
-    //   };
+        //   $scope.refreshServicesOverviewData=function (){
+        //     $http.get("data/raw-services-overview-2.json").then(function (response) {
+        //       $scope.servicesOverviewData = response.data;
+        //     }, onError);
+        //   };
 
-    var onError = function (response) {
-      $scope.error = response.status;
-      $log.log(
-        "Error : status " +
-          response.status +
-          " data " +
-          response.data +
-          " headers " +
-          response.headers +
-          " config " +
-          response.config
-      );
-    };
+        var onError = function(response) {
+            $scope.error = response.status;
+            $log.log(
+                "Error : status " +
+                response.status +
+                " data " +
+                response.data +
+                " headers " +
+                response.headers +
+                " config " +
+                response.config
+            );
+        };
 
-    //   $scope.refreshServicesOverviewData();
-  },
+        //   $scope.refreshServicesOverviewData();
+    },
 ]);
