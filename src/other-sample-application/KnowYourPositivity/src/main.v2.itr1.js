10d1a8245bf8267e567cdf6b5c11214e46d3dceb
Riku Ishwariya Chandra Das Gupta <sameer.geeee@gmail.com>
2025-02-17 22:16:02 +0530

    commit

--- src/other-sample-application/KnowYourPositivity/src/main.v2.itr1.js
+++ src/other-sample-application/KnowYourPositivity/src/main.v2.itr1.js
@@ -1,7 +1,5 @@
 import pages from "./utils/PageData.repository.js";
 
-// console.log("Pages: ",pages);
-
 // Function to generate a single list item
 const createListItem = ({ name, link, children }) => {
   const linkStr = link ? `<a href="${link}">${name}</a> ` : `<b>${name}</b>`;
@@ -28,7 +26,6 @@ const getLinks = (pages = []) => pages.map(createListItem).join("");
 const ulElement = document.getElementById("nav-links-on-home-page");
 
 ulElement.innerHTML += getLinks(pages);
-
 
 // Dark-Light Mode Toggle
 const themeToggleBtn = document.getElementById("theme-toggle");
